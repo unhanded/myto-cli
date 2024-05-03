1ae97f605e0959c7f9b9cdc791ae6019d357127e
Lukas Sondell <sondelln@gmail.com>
2024-05-03 13:04:48 +0200

    Moved commandline tool from mytomake repo

---
+++ cmd/myto/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/unhanded/myto-cli/internal/util"
+	"github.com/unhanded/mytomake/pkg/mytomake"
+)
+
+func main() {
+	var filepath string
+	var dataDir string
+	flag.StringVar(&filepath, "f", "", "the selected myto file")
+	flag.StringVar(&dataDir, "d", "$HOME/.myto", "myto data directory")
+
+	flag.CommandLine.Usage = func() {
+		util.Banner()
+
+		fmt.Print("\tUsage: myto -f <file>\n\n")
+		fmt.Print("\tExample: myto -f ./message.myto\n\n")
+	}
+
+	flag.Parse()
+
+	if filepath == "" {
+		flag.CommandLine.Usage()
+		os.Exit(1)
+	}
+	myto := mytomake.NewMyto(dataDir)
+
+	if filepath == "" {
+		b, bErr := io.ReadAll(os.Stdin)
+		if bErr != nil {
+			panic(bErr)
+		}
+		res, err := myto.FromBytes(b)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", res)
+	} else {
+		res, err := myto.FromFile(filepath)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", res)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/unhanded/myto-cli
+
+go 1.22.2
+
+require github.com/unhanded/mytomake v0.0.2
+
+require github.com/google/uuid v1.6.0 // indirect

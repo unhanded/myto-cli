19c2ee06415f98b66a151e06b3a0dc3a5dfa17df
Lukas Sondell <sondelln@gmail.com>
2024-05-18 12:40:28 +0200

    Moved main code into internal

---
+++ internal/application/core.go
@@ -0,0 +1,49 @@
+package application
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/unhanded/myto-cli/internal/util"
+	"github.com/unhanded/mytomake/pkg/mytomake"
+)
+
+func RunApp(){
+	var filepath string
+	var dataDir string
+	flag.StringVar(&filepath, "f", "", "the selected myto file")
+	flag.StringVar(&dataDir, "d", "$HOME/.myto", "myto data directory")
+
+	flag.CommandLine.Usage = func() {
+		util.Banner()
+
+		fmt.Print("\tUsage: myto -f <file> [-d <myto data directory> (Default: $HOME/.myto)]\n\n")
+		fmt.Print("\tExample: myto -f ./message.myto\n\n")
+	}
+
+	flag.Parse()
+
+	myto := mytomake.NewMyto(dataDir)
+
+	if filepath == "" {
+		b, bErr := io.ReadAll(os.Stdin)
+		if bErr != nil {
+			panic(bErr)
+		}
+		res, err := myto.FromBytes(b)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", res)
+	} else {
+		res, err := myto.FromFile(filepath)
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", res)
+	}
+}
--- main.go
+++ main.go
@@ -1,49 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
-	"os"
-
-	"github.com/unhanded/myto-cli/internal/util"
-	"github.com/unhanded/mytomake/pkg/mytomake"
+	"github.com/unhanded/myto-cli/internal/application"
 )
 
 func main() {
-	var filepath string
-	var dataDir string
-	flag.StringVar(&filepath, "f", "", "the selected myto file")
-	flag.StringVar(&dataDir, "d", "$HOME/.myto", "myto data directory")
-
-	flag.CommandLine.Usage = func() {
-		util.Banner()
-
-		fmt.Print("\tUsage: myto -f <file>\n\n")
-		fmt.Print("\tExample: myto -f ./message.myto\n\n")
-	}
-
-	flag.Parse()
-
-	myto := mytomake.NewMyto(dataDir)
-
-	if filepath == "" {
-		b, bErr := io.ReadAll(os.Stdin)
-		if bErr != nil {
-			panic(bErr)
-		}
-		res, err := myto.FromBytes(b)
-
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", res)
-	} else {
-		res, err := myto.FromFile(filepath)
-
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("%s\n", res)
-	}
+	application.RunApp()
 }
